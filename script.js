2de88157834e8b4f7be622c128db39edb8936fbc
danishmemon55 <danishmemon005500@gmail.com>
2024-10-06 14:41:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var newtyped = new Typed(".loop",{
+
+    strings: ["Frontend Developer","Graphic Designer"],
+
+    typeSpeed:150,
+    backSpeed:150,
+    looped:true
+})
+// Get all accordion headers
+const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+// Loop through each header and add a click event listener
+accordionHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+        // Toggle the active class on the clicked header
+        header.classList.toggle('active');
+
+        // Get the next sibling element (accordion content)
+        const content = header.nextElementSibling;
+
+        // Check if content is currently displayed
+        if (content.style.display === 'block') {
+            // If it's displayed, hide it
+            content.style.display = 'none';
+        } else {
+            // If it's hidden, display it
+            content.style.display = 'block';
+        }
+    });
+});
